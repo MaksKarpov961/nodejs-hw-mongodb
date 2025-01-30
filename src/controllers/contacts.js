785287194602d416bfe3aca7b998da92c3d661d5
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2025-01-30 14:55:46 +0200

    sort

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -9,10 +9,17 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
-  const contacts = await getAllcontacts({ page, perPage });
+  const { sortOrder, sortBy } = parseSortParams(req.query);
+  const contacts = await getAllcontacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+  });
 
   res.status(200).json({
     status: 200,
